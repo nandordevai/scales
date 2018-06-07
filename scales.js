89b6dcbb81576830296a38a99d0fe5889d69e76c
Nandor Devai <mail@devainandor.com>
2018-06-07 22:10:43 +0200

    well, fuck standard

--- scales.js
+++ scales.js
@@ -1,117 +1,117 @@
 const scales = [
-    {
-        names: ['Diminished'],
-        notes: [0, 1, 3, 4, 6, 7, 9, 10],
-    },
-    {
-        names: ['Dorian'],
-        notes: [0, 2, 3, 5, 7, 9, 10],
-    },
-    {
-        names: ['Bhairav', 'Double harmonic major'],
-        notes: [0, 1, 4, 5, 7, 8, 11],
-    },
-    {
-        names: ['Buselik', 'Harmonic Minor'],
-        notes: [0, 2, 3, 5, 7, 8, 11],
-    },
-    {
-        names: ['Hijaz'],
-        notes: [0, 1, 4, 5, 7, 8, 10, 11],
-    },
-    {
-        names: ['Hirojoshi'],
-        notes: [0, 2, 3, 7, 8],
-    },
-    {
-        names: ['Hungarian Minor'],
-        notes: [0, 2, 3, 6, 7, 8, 11],
-    },
-    {
-        names: ['In-Sen'],
-        notes: [0, 1, 5, 7, 10],
-    },
-    {
-        names: ['Iwato'],
-        notes: [0, 1, 5, 6, 10],
-    },
-    {
-        names: ['Kumoi'],
-        notes: [0, 2, 3, 7, 9],
-    },
-    {
-        names: ['Locrian'],
-        notes: [0, 1, 3, 5, 6, 8, 10],
-    },
-    {
-        names: ['Lydian'],
-        notes: [0, 2, 4, 6, 7, 9, 11],
-    },
-    {
-        names: ['Major Pentatonic'],
-        notes: [0, 2, 4, 7, 9],
-    },
-    {
-        names: ['Major'],
-        notes: [0, 2, 4, 5, 7, 9, 11],
-    },
+  {
+    names: ['Diminished'],
+    notes: [0, 1, 3, 4, 6, 7, 9, 10],
+  },
+  {
+    names: ['Dorian'],
+    notes: [0, 2, 3, 5, 7, 9, 10],
+  },
+  {
+    names: ['Bhairav', 'Double harmonic major'],
+    notes: [0, 1, 4, 5, 7, 8, 11],
+  },
+  {
+    names: ['Buselik', 'Harmonic Minor'],
+    notes: [0, 2, 3, 5, 7, 8, 11],
+  },
+  {
+    names: ['Hijaz'],
+    notes: [0, 1, 4, 5, 7, 8, 10, 11],
+  },
+  {
+    names: ['Hirojoshi'],
+    notes: [0, 2, 3, 7, 8],
+  },
+  {
+    names: ['Hungarian Minor'],
+    notes: [0, 2, 3, 6, 7, 8, 11],
+  },
+  {
+    names: ['In-Sen'],
+    notes: [0, 1, 5, 7, 10],
+  },
+  {
+    names: ['Iwato'],
+    notes: [0, 1, 5, 6, 10],
+  },
+  {
+    names: ['Kumoi'],
+    notes: [0, 2, 3, 7, 9],
+  },
+  {
+    names: ['Locrian'],
+    notes: [0, 1, 3, 5, 6, 8, 10],
+  },
+  {
+    names: ['Lydian'],
+    notes: [0, 2, 4, 6, 7, 9, 11],
+  },
+  {
+    names: ['Major Pentatonic'],
+    notes: [0, 2, 4, 7, 9],
+  },
+  {
+    names: ['Major'],
+    notes: [0, 2, 4, 5, 7, 9, 11],
+  },
 
-    {
-        names: ['Melodic Minor ascending'],
-        notes: [0, 2, 3, 5, 7, 9, 11],
-    },
-    {
-        names: ['Melodic Minor descending'],
-        notes: [0, 2, 4, 5, 7, 9, 10],
-    },
-    {
-        names: ['Minor Blues'],
-        notes: [0, 3, 5, 6, 7, 10],
-    },
-    {
-        names: ['Minor Pentatonic'],
-        notes: [0, 3, 5, 7, 10],
-    },
-    {
-        names: ['Minor'],
-        notes: [0, 2, 3, 5, 7, 8, 10],
-    },
-    {
-        names: ['Mixolydian'],
-        notes: [0, 2, 4, 5, 7, 9, 10],
-    },
-    {
-        names: ['Pelog'],
-        notes: [0, 1, 3, 4, 7, 8],
-    },
-    {
-        names: ['Persian'],
-        notes: [0, 1, 4, 5, 6, 8, 11],
-    },
-    {
-        names: ['Kurd', 'Phrygian'],
-        notes: [0, 1, 3, 5, 7, 8, 10],
-    },
-    {
-        names: ['Minor Gypsy', 'Phrygian dominant'],
-        notes: [0, 1, 4, 5, 7, 8, 10],
-    },
-    {
-        names: ['Spanish'],
-        notes: [0, 1, 3, 4, 5, 6, 8, 10],
-    },
-    {
-        names: ['Super Locrian'],
-        notes: [0, 1, 3, 4, 6, 8, 10],
-    },
-    {
-        names: ['Whole Tone'],
-        notes: [0, 2, 4, 6, 8, 10],
-    },
-    {
-        names: ['Whole-half'],
-        notes: [0, 2, 3, 5, 6, 8, 9, 11],
-    },
+  {
+    names: ['Melodic Minor ascending'],
+    notes: [0, 2, 3, 5, 7, 9, 11],
+  },
+  {
+    names: ['Melodic Minor descending'],
+    notes: [0, 2, 4, 5, 7, 9, 10],
+  },
+  {
+    names: ['Minor Blues'],
+    notes: [0, 3, 5, 6, 7, 10],
+  },
+  {
+    names: ['Minor Pentatonic'],
+    notes: [0, 3, 5, 7, 10],
+  },
+  {
+    names: ['Minor'],
+    notes: [0, 2, 3, 5, 7, 8, 10],
+  },
+  {
+    names: ['Mixolydian'],
+    notes: [0, 2, 4, 5, 7, 9, 10],
+  },
+  {
+    names: ['Pelog'],
+    notes: [0, 1, 3, 4, 7, 8],
+  },
+  {
+    names: ['Persian'],
+    notes: [0, 1, 4, 5, 6, 8, 11],
+  },
+  {
+    names: ['Kurd', 'Phrygian'],
+    notes: [0, 1, 3, 5, 7, 8, 10],
+  },
+  {
+    names: ['Minor Gypsy', 'Phrygian dominant'],
+    notes: [0, 1, 4, 5, 7, 8, 10],
+  },
+  {
+    names: ['Spanish'],
+    notes: [0, 1, 3, 4, 5, 6, 8, 10],
+  },
+  {
+    names: ['Super Locrian'],
+    notes: [0, 1, 3, 4, 6, 8, 10],
+  },
+  {
+    names: ['Whole Tone'],
+    notes: [0, 2, 4, 6, 8, 10],
+  },
+  {
+    names: ['Whole-half'],
+    notes: [0, 2, 3, 5, 6, 8, 9, 11],
+  },
 ];
 
 export { scales };
