d7e349c15768d71f0acd179b1670e28ad222b8e8
Nandor Devai <mail@devainandor.com>
2019-02-12 22:44:29 +0100

    show intervals & scales

--- src/App.js
+++ src/App.js
@@ -8,8 +8,40 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{scales[2].names.join(', ')}</h1>
+        <div className="intervals">{scales[2].intervals.join(', ')}</div>
+        <div className="scale">{this.createScale(scales[2].intervals).join(', ')}</div>
       </div>
     );
+  }
+
+  createScale(intervals, root = 'C') {
+    const allNotes = [
+      'C',
+      ['C#', 'Db'],
+      'D',
+      ['D#', 'Eb'],
+      'E',
+      'F',
+      ['F#', 'Gb'],
+      'G',
+      ['G#', 'Ab'],
+      'A',
+      ['A#', 'Bb'],
+      'B'
+    ];
+    const extendedScale = allNotes.concat(allNotes);
+    const transposeBy = allNotes.indexOf(root);
+    return intervals
+      .map(_ => extendedScale[_ + transposeBy])
+      .map((_, i) => {
+        if (!Array.isArray(_)) {
+          return _;
+        }
+        if ((i < intervals.length - 1) && (_[1][0] === extendedScale[intervals[i + 1] + transposeBy])) {
+          return _[0];
+        }
+        return _[1];
+      });
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+let div = null;
+
+beforeEach(() => {
+  div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('shows the names of the selected scale', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
   expect(div.querySelector('h1').textContent).toBe('Bhairav, Double harmonic major');
+});
+
+it('shows the intervals of the selected scale', () => {
+  expect(div.querySelector('.intervals').textContent).toBe('0, 1, 4, 5, 7, 8, 11');
+});
+
+it('shows the notes of the selected scale', () => {
+  expect(div.querySelector('.scale').textContent).toBe('C, Db, E, F, G, Ab, B');
+});
+
+it('creates a scale from intervals', () => {
+  expect(App.prototype.createScale([0, 1])).toEqual(['C', 'Db']);
+});
+
+it('creates a scale with a given root note', () => {
+  expect(App.prototype.createScale([0, 1], 'D')).toEqual(['D', 'Eb']);
+  expect(App.prototype.createScale([0, 2, 3, 5, 7, 9, 10], 'D')).toEqual(['D', 'E', 'F', 'G', 'A', 'B', 'C']);
+});
+
+it('creates a scale with proper sharp and flat notes', () => {
+  expect(App.prototype.createScale([0, 2, 3], 'E')).toEqual(['E', 'F#', 'G']);
 });
